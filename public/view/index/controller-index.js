198037839c6e79976d5c68e309cc260738f73555
mazhenxiao <mazhenxiao@gmail.com>
2018-07-04 09:39:29 +0800

    保存数据

--- public/view/index/controller-index.js
+++ public/view/index/controller-index.js
@@ -1,6 +1,6 @@
 import * as service from "@service/service-index.js";
 export default {
-  //我在master上的注释上的注释
+  //我在dev上的注释上的注释 
   async getData(){
        let {li:data} = await service.getData();
         this.dispatch({
